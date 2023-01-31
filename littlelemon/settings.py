b2fa7a6986d5ef48faf0d7dc21d0fc38a1d00df1
Luis Zanabria <lulaamaj7@hotmail.com>
2023-01-31 17:38:16 -0600

    Connection to mysql and mysqlite3 db removed

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,7 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': BASE_DIR/'my.cnf',
+            'read_default_file': str(BASE_DIR / 'my.cnf'),
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
