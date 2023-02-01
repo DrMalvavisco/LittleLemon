718fb09b24d62c1b5153ab8f78ef3ea8d162306f
Luis Zanabria <lulaamaj7@hotmail.com>
2023-01-31 20:36:30 -0600

    Unitest added

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Menu, Booking
+from .models import Menu, Booking, MenuItem
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(Menu)
+admin.site.register(MenuItem)
