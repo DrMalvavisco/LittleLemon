ee5c8956193bbff861f7fdd23dfa3f41531bb4d9
Luis Zanabria <lulaamaj7@hotmail.com>
2023-01-31 20:54:05 -0600

    Version 1.0

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('booking', views.BookingView.as_view()),
     path('booking/<int:pk>', views.SingleBookingView.as_view()),
     path('menu', views.MenuView.as_view()),
