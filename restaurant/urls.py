43c846deb038cb9087c94ebd59bba56d06c23441
Luis Zanabria <lulaamaj7@hotmail.com>
2023-01-31 18:34:14 -0600

    Added serializers and Generic views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('booking', views.BookingView.as_view()),
+    path('booking/<int:pk>', views.SingleBookingView.as_view()),
+    path('menu', views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
     # path('about/', views.about, name='about'),
     # path('book/', views.book, name='book'),
     # path('menu/', views.menu, name='menu'),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# Example using APIView class
+# class BookingView(APIView):
+    
+#     def get(self, request):
+#         items = Booking.objects.all()
+#         serializer = BookingSerializer(items, many=True)
+#         return Response(serializer.data)
+
+class BookingView(ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class SingleBookingView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
